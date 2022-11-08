806b97f39c47db03a576a815cf59d862245ae4cc
francis-njoku <njokuchimauche@gmail.com>
2022-11-08 15:34:12 +0100

    Pust model

--- authentication/models.py
+++ authentication/models.py
@@ -11,16 +11,16 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, username, firstname, lastname, address, referral_code, phone, email, password=None):
+    def create_user(self, username, email, password=None):
         '''if username is None:
             raise TypeError('Users should have a username')
             '''
         if email is None:
             raise TypeError('Users should have a Email')
 
-        user = self.model(username=username, firstname=firstname, address=address, lastname=lastname,
-                          phone=phone, referral_code=referral_code, email=self.normalize_email(email))
-        #user = self.model(email=self.normalize_email(email))
+        # user = self.model(username=username, firstname=firstname, address=address, lastname=lastname,
+            # phone=phone, referral_code=referral_code, email=self.normalize_email(email))
+        user = self.model(username=username, email=self.normalize_email(email))
         user.set_password(password)
         user.save()
         return user
