b4e763aee26fb2dd6e1c4bcd1cb3cc1705c87f48
francis-njoku <njokuchimauche@gmail.com>
2022-11-08 04:52:54 +0100

    allowed host

--- property/settings.py
+++ property/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1',
+                 'yields-room.herokuapp.com', 'https://yields-room.herokuapp.com']
 
 AUTH_USER_MODEL = 'authentication.User'
 # Application definition
