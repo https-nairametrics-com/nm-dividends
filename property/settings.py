d540a35fac70a43684cedb87715be55a39e59538
francis-njoku <njokuchimauche@gmail.com>
2022-11-08 14:19:28 +0100

    auto field

--- property/settings.py
+++ property/settings.py
@@ -223,3 +223,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
