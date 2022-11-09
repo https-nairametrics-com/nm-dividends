c71370dee68d794b737fed6962f232061bc7adb6
francis-njoku <njokuchimauche@gmail.com>
2022-11-09 05:49:02 +0100

    Update investment admin

--- investment/admin.py
+++ investment/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Investment, InvestmentRoom, Gallery, Investors,
+from .models import Investment, InvestmentRoom, Gallery, Investors
 
 
 class InvestmentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'auth_provider', 'created_at']
+    list_display = ['name', 'description', 'created_at']
 
 
-admin.site.register(InvestmentRoom, Investment)
+admin.site.register(Investment, InvestmentAdmin)
+admin.site.register(InvestmentRoom)
+admin.site.register(Gallery)
+admin.site.register(Investors)
