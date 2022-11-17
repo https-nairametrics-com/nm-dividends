e6a32c8cb4b0d34b93b30ba5858cf1d6d407acec
francis-njoku <njokuchimauche@gmail.com>
2022-11-17 14:57:53 +0100

    Create helpers class

---
+++ investment/helpers.py
@@ -0,0 +1,6 @@
+def modify_input_for_multiple_files(investment, gallery, is_featured):
+    dict = {}
+    dict['investment'] = investment
+    dict['gallery'] = gallery
+    dict['is_featured'] = is_featured
+    return dict
