0630dd20e3f6e949a1195365dc30a75855b0653f
francis-njoku <njokuchimauche@gmail.com>
2022-12-12 05:29:35 +0100

    add fields to investment model

--- investment/models.py
+++ investment/models.py
@@ -13,13 +13,57 @@ def upload_to(instance, filename):
     return 'posts/{filename}'.format(filename=filename)
 
 
-class InvestmentRoom(models.Model):
+class Currency(models.Model):
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class DealType(models.Model):
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MainRoom(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, null=True, unique=True)
     description = models.TextField(null=True)
     is_verified = models.BooleanField(default=False)
     created_by = models.ForeignKey(
         to=User, on_delete=models.CASCADE, related_name='created_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('room_detail', kwargs={'slug': self.slug})
+
+
+class InvestmentRoom(models.Model):
+    name = models.CharField(max_length=255)
+    main_category = models.ForeignKey(
+        to=MainRoom, null=True, on_delete=models.CASCADE, related_name='main_room')
+    slug = models.SlugField(max_length=255, null=True, unique=True)
+    description = models.TextField(null=True)
+    is_verified = models.BooleanField(default=False)
+    created_by = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name='main_created_by')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -42,16 +86,28 @@ class Investment(models.Model):
     slug = models.SlugField(max_length=255, null=True, unique=True)
     description = models.TextField()
     location = models.CharField(max_length=255, null=True)
+    video = models.CharField(max_length=255, null=True)
     room = models.ForeignKey(to=InvestmentRoom, on_delete=models.CASCADE)
     period = models.ForeignKey(
         to=Period, on_delete=models.CASCADE, related_name='investment_period')
+    currency = models.ForeignKey(
+        to=Currency, null=True, on_delete=models.CASCADE, related_name='investment_currency')
+    dealtype = models.ForeignKey(
+        to=DealType, null=True, on_delete=models.CASCADE, related_name='investment_dealtype')
     amount = models.IntegerField(null=True)
+    volume = models.IntegerField(null=True)
+    offer_price = models.IntegerField(null=True)
+    bid_price = models.IntegerField(null=True)
+    spot_price = models.IntegerField(null=True)
+    unit_price = models.IntegerField(null=True)
     roi = models.CharField(max_length=255)
     annualized = models.CharField(max_length=255)
     risk = models.ForeignKey(to=Risk, on_delete=models.CASCADE)
     features = models.TextField(null=True)
     is_verified = models.BooleanField(default=False)
     is_closed = models.BooleanField(default=False)
+    start_date = models.DateTimeField(null=True)
+    end_date = models.DateTimeField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
