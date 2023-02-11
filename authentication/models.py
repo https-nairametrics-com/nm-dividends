fba0d20fd4987655f5e396eb54dca27392601f57
francis-njoku <njokuchimauche@gmail.com>
2023-02-11 13:16:20 +0100

    Add image destination to model

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,7 @@
 from email.policy import default
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
 from django.contrib.auth.models import (
