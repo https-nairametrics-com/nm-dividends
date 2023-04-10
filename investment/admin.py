8e479be583c1a2f560008a670156a5c71ab0945e
francis-njoku <njokuchimauche@gmail.com>
2023-04-10 16:37:21 +0100

    update investor admin

--- investment/admin.py
+++ investment/admin.py
@@ -7,12 +7,14 @@ from .models import Sponsor, SponsorInvestment, Currency, DealType, MainRoom, In
 class InvestmentAdmin(admin.ModelAdmin):
     list_display = ['name', 'description', 'created_at']
 
+class InvestorAdmin(admin.ModelAdmin):
+    list_display = ['amount', 'investment', 'created_at']
 
 admin.site.register(Investment, InvestmentAdmin)
 admin.site.register(MainRoom)
 admin.site.register(InvestmentRoom)
 admin.site.register(Gallery)
-admin.site.register(Investors)
+admin.site.register(Investors, InvestorAdmin)
 admin.site.register(Currency)
 admin.site.register(DealType)
 admin.site.register(Sponsor)
